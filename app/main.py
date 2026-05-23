d46b7be71be5aba021ef3f2e36b148bf8e429a76
nakul1999 <agnakul77@gmail.com>
2026-05-23 17:37:47 +0100

    CORS issue 2.0

--- app/main.py
+++ app/main.py
@@ -28,12 +28,8 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        FRONTEND_URL,
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
