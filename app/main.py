b77667aefebe1906c8d2ca4c8504c06e7abe5745
nakul1999 <agnakul77@gmail.com>
2026-05-23 17:37:47 +0100

    CORS issue

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        FRONTEND_URL
+        FRONTEND_URL,
     ],
     allow_credentials=True,
     allow_methods=["*"],
